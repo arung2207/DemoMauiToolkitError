02019297f431bec1903548686588b6fb6da18e63
Arun Gupta <arung2207@gmail.com>
2023-06-02 13:06:08 +0530

    Writing an explicit converter seems to make it work!

---
+++ DemoErrorMauiApp/Converters/CandidatesConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace DemoErrorMauiApp.Converters;
+public class CandidatesConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var v = (List<string>)value;
+
+        if (v != null && v.Any())
+        {
+            return value;
+        }
+
+        return new List<string> { "" };
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
